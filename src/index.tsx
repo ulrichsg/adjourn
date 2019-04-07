7529e6e3cc0ce5206c1a37ad384a073d2db95f9b
Ulrich Schmidt-Goertz <ulrich@schmidt-goertz.de>
2019-04-07 22:03:00 +0200

    Add support for locally hosted webfonts
    see KyleAMathews/typefaces

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import {render} from 'react-dom';
 import styled from 'styled-components';
+// tslint:disable-next-line no-var-requires
+require('typeface-open-sans');
 
 const Hello = styled.div`
+  font-family: "Open Sans", serif;
   font-weight: bold;
   color: red;
 `;
--- webpack.config.js
+++ webpack.config.js
@@ -2,25 +2,32 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index',
+  entry: './src/index.tsx',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'app.bundle.js'
   },
-  resolve: {
-    extensions: ['.ts', '.tsx', '.js', '.json']
-  },
   module: {
-    rules: [{
-      test: /\.(ts|js)x?$/,
-      exclude: /node_modules/,
-      loader: 'babel-loader'
-    }]
+    rules: [
+      {
+        test: /\.(ts|js)x?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.css$/,
+        use: ['file-loader?name=style.css', 'extract-loader', 'css-loader']
+      },
+      {
+        test: /\.(svg|ttf|woff|woff2)$/,
+        loader: 'file-loader?name=[name].[ext]&outputPath=files',
+      },
+    ]
   },
   plugins: [
     new HtmlWebpackPlugin({
       filename: 'index.html',
       template: './src/index.html'
-    })
+    }),
   ]
 };
